a1b46b5411228115ab48d09678d011dbd99a726a
jneira <atreyu.bbb@gmail.com>
2017-04-19 06:25:49 +0200

    testing nullAddr

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,7 @@ import qualified Data.ByteString              as B
 import Network.Wai
 import GHC.Base
 import Data.Word (Word8)
-import Foreign.Ptr (Ptr)
+import Foreign.Ptr (Ptr,nullPtr)
 import Foreign.ForeignPtr (ForeignPtr,newForeignPtr_,withForeignPtr)
 import qualified Data.ByteString.Internal as BSInt
 import Java
@@ -13,5 +13,8 @@ foreign import java unsafe "@static Utils.trace"
    trace :: Ptr Word8 -> Int -> Int -> IO ()
 
 main = do
+  print nullPtr
   let (fptr,offset,length) = BSInt.toForeignPtr B.empty
-  withForeignPtr fptr $ \ ptr -> trace ptr offset length
+  withForeignPtr fptr $ \ ptr -> do
+    trace ptr offset length
+    print ptr
