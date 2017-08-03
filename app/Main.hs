d04dc848feb0d3ad5cf0bff1b2543d59a4e38d0f
jneira <atreyu.bbb@gmail.com>
2017-08-04 00:32:32 +0200

    Testing allocating an empty buffer wit MemoryManager

--- app/Main.hs
+++ app/Main.hs
@@ -1,33 +1,12 @@
 {-# LANGUAGE MagicHash,TypeFamilies,DataKinds,
     FlexibleContexts,OverloadedStrings #-}
 module Main where
-import Network.Wai
-import Network.HTTP.Types as H
-import Network.Socket
-import Data.Word (Word8)
-import Data.Text as T
-import Data.Text.Encoding
-import Data.Text.Encoding.Error
-import qualified Data.ByteString              as B
-import qualified Data.ByteString.Internal as BSInt
-import Foreign.Ptr (Ptr,nullPtr)
-import Foreign.ForeignPtr (ForeignPtr,newForeignPtr_,withForeignPtr)
 import GHC.Base
 import Java
 
-foreign import java unsafe "@static Utils.trace"
-   trace :: Ptr Word8 -> Int -> Int -> IO ()
+foreign import java unsafe "@static Utils.testEmptyBuffer"
+  testEmptyBuffer :: IO ()
 
-main = do
-  putStrLn $ "nullPtr: " ++ show nullPtr
-  let (fptr,offset,length) = BSInt.toForeignPtr B.empty
-  withForeignPtr fptr $ \ ptr -> do
-    trace ptr offset length
-    putStrLn $ "ptr: " ++ show ptr
-    putStrLn $ "ptr == nullPtr " ++ (show $ nullPtr == ptr)
-  putStrLn $ "ByteString.empty: " ++  show B.empty
-  putStrLn $ "Wai request: " ++ show defaultRequest
-  print $ SockAddrInet6 0 0 (tupleToHostAddress6 (0,0,0,0,0,0,0,0)) 0 
-  print $ H.decodePathSegments "/my/path"
-  print $ H.parseQuery "a=1&b=2"
+main = testEmptyBuffer
+  
 
--- java/Utils.java
+++ java/Utils.java
@@ -1,9 +1,19 @@
 import java.nio.ByteBuffer;
+import eta.runtime.io.MemoryManager;
 
 public class Utils {
     public static void trace(ByteBuffer buf,int offset, int length) {
         System.out.println("Ptr: "+buf);
-        System.out.println("Ofset: "+offset);
+        System.out.println("Offset: "+offset);
         System.out.println("Length: "+length);
+    }
+
+    public static void testEmptyBuffer () {
+        System.out.println("Testing allocate an empty buffer with MemoryManager");
+        byte[] bytes = new byte[] {};
+        long address = MemoryManager.allocateBuffer(bytes.length,false);
+        System.out.println("Address: "+address);
+        ByteBuffer buf = MemoryManager.getBoundedBuffer(address);
+        System.out.println("Buffer: "+buf);
     }
 }
