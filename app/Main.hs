6e8e1e4480bbd27c5777b5a74b65f9549cf5cc79
jneira <atreyu.bbb@gmail.com>
2017-09-13 14:57:10 +0200

    testing unicode chars

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,17 @@
 {-# LANGUAGE MagicHash,TypeFamilies,DataKinds,
     FlexibleContexts,OverloadedStrings #-}
 module Main where
-import qualified Data.ByteString              as B
-import Network.HTTP.Types as H
-import Network.Wai
-import Network.Socket
-import GHC.Base
-import Data.Word (Word8)
-import Foreign.Ptr (Ptr,nullPtr)
-import Foreign.ForeignPtr (ForeignPtr,newForeignPtr_,withForeignPtr)
-import qualified Data.ByteString.Internal as BSInt
-import Java
+import Data.Text
+import Data.Text.Encoding
+import Data.Text.Encoding.Error
 
-foreign import java unsafe "@static Utils.trace"
-   trace :: Ptr Word8 -> Int -> Int -> IO ()
 
 main = do
-  putStrLn $ "nullPtr: " ++ show nullPtr
-  let (fptr,offset,length) = BSInt.toForeignPtr B.empty
-  withForeignPtr fptr $ \ ptr -> do
-    trace ptr offset length
-    putStrLn $ "ptr: " ++ show ptr
-    putStrLn $ "ptr == nullPtr " ++ (show $ nullPtr == ptr)
-  putStrLn $ "ByteString.empty: " ++  show B.empty
-  putStrLn $ "Wai request: " ++ show defaultRequest
-  print $ SockAddrInet6 0 0 (tupleToHostAddress6 (0,0,0,0,0,0,0,0)) 0 
-  --print $ H.decodePathSegments "/my/path"
-  print $ H.parseQuery "a=1&b=2"
+  putStrLn "\xf1"
+  putStrLn "ñ"
+  putStrLn "\xc3\xb1"
+  print $ decodeUtf8With lenientDecode "ñ"
+  print $ decodeUtf8With lenientDecode "añ"
+  print $ decodeUtf8With strictDecode "ñ"
+
+  
