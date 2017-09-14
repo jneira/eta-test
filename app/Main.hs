802a4d1ac5f577175c31ad81ed6662d95666aaf6
jneira <atreyu.bbb@gmail.com>
2017-09-14 14:19:11 +0200

    testing weird behaviour with file.encoding=UTF-8

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,9 @@
 {-# LANGUAGE MagicHash,TypeFamilies,DataKinds,
     FlexibleContexts,OverloadedStrings #-}
 module Main where
-import Data.Text
-import Data.Text.Encoding
-import Data.Text.Encoding.Error
 
 
-main = do
-  putStrLn "\xf1"
-  putStrLn "ñ"
-  putStrLn "\xc3\xb1"
-  print $ decodeUtf8With lenientDecode "ñ"
-  print $ decodeUtf8With lenientDecode "añ"
-  print $ decodeUtf8With strictDecode "ñ"
+main = putStrLn "ñaca ñeca ñoca ñica ñuca ñace ñece ñoce ñice ñuce"
+
 
   
