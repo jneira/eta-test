ad0654dc6385208d6b490c12cb246d228f12b477
jneira <atreyu.bbb@gmail.com>
2017-09-22 06:25:19 +0200

    launch script with env vars

--- eta-test.cmd
+++ eta-test.cmd
@@ -1,6 +1,4 @@
-﻿@echo off
+@echo off
 set DIR=%~dp0
-for /f "tokens=*" %%i in ('powershell -Command "[console]::OutputEncoding.BodyName"') do set cp=%%i
-set cp=%cp:﻿=%
-echo %cp%
-java -Dfile.encoding=%cp% -classpath "%DIR%\eta-test.launcher.jar" eta.main %*
+java  -classpath "%DIR%\eta-test.launcher.jar" %JAVA_ARGS% %JAVA_OPTS% eta.main %*
+
--- eta-test2.cmd
+++
@@ -1,6 +0,0 @@
-﻿@echo off
-set DIR=%~dp0
-for /f "tokens=2 delims=:" %i in ('chcp') do set cp= %i
-chcp 65001
-java -Dfile.encoding=UTF-8 -classpath "%DIR%\eta-test.launcher.jar" eta.main %*
-chcp %cp%
