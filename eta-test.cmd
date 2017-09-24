27b39cc815451a1c6d7ddc25bf413c989ec1eed1
jneira <atreyu.bbb@gmail.com>
2017-09-25 00:17:52 +0200

    win launcher script with env vars

--- eta-test.cmd
+++ eta-test.cmd
@@ -1,4 +1,10 @@
 @echo off
 set DIR=%~dp0
-java  -classpath "%DIR%\eta-test.launcher.jar" %JAVA_ARGS% %JAVA_OPTS% eta.main %*
+set ETA_JAVA_PROG=java.exe
+if defined JAVA_HOME (
+  set JAVA_HOME=%JAVA_HOME:"=%
+  set ETA_JAVA_PROG=%JAVA_HOME%\bin\%ETA_JAVA_PROG%
+)
+
+"%ETA_JAVA_PROG%"  -classpath "%DIR%\eta-test.launcher.jar" %JAVA_ARGS% %JAVA_OPTS% eta.main %*
 
