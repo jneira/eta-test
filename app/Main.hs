90c3e9e24a589aea4726d88f4f33ba7e0585dc77
jneira <atreyu.bbb@gmail.com>
2017-10-02 14:45:30 +0200

    more tests

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@
     FlexibleContexts,OverloadedStrings #-}
 module Main where
 
-main = putStrLn "नमस्ते" >> putStrLn "你好" >>
-       putStrLn "привет" >> putStrLn "Capital EX 🦎's first module!"
+main = putStrLn "cañá" >> putStrLn "नमस्ते" >> putStrLn "你好" >>
+       putStrLn "привет" -- >> putStrLn "Capital EX 🦎's first module!"
 
 
--- eta-test.cmd
+++ eta-test.cmd
@@ -1,10 +1,12 @@
 @echo off
 set DIR=%~dp0
-set ETA_JAVA_PROG=java.exe
-if defined JAVA_HOME (
-  set JAVA_HOME=%JAVA_HOME:"=%
-  set ETA_JAVA_PROG=%JAVA_HOME%\bin\%ETA_JAVA_PROG%
-)
-
-"%ETA_JAVA_PROG%" %JAVA_ARGS% %JAVA_OPTS% %ETA_ARGS% -classpath "%DIR%\eta-test.launcher.jar" eta.main %*
+if defined JAVA_HOME goto findJavaFromJavaHome
+set ETA_JAVA_CMD=java.exe
+goto init
+:findJavaFromJavaHome
+set ETA_JAVA_HOME=%JAVA_HOME:"=%
+set ETA_JAVA_CMD=%ETA_JAVA_HOME%\bin\java.exe
+:init
+"%ETA_JAVA_CMD%" -version
+rem "%ETA_JAVA_CMD%" %JAVA_ARGS% %JAVA_OPTS% %ETA_JAVA_ARGS% -classpath "%DIR%\eta-test.launcher.jar" eta.main %*
 
