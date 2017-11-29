cbce887201492cc175445f607e46bae7789927e6
jneira <atreyu.bbb@gmail.com>
2017-11-29 10:09:04 +0100

    remove test getBuffer

--- java/Utils.java
+++ java/Utils.java
@@ -8,14 +8,4 @@ public class Utils {
         System.out.println("Length: "+length);
     }
 
-    public static void testEmptyBuffer () {
-        System.out.println("Testing allocate an empty buffer with MemoryManager");
-        byte[] bytes = new byte[] {};
-        long address = MemoryManager.allocateBuffer(bytes.length,false);
-        System.out.println("Address: "+address);
-        ByteBuffer buf = MemoryManager.getBuffer(address);
-        System.out.println("Buffer: "+buf);
-        buf = MemoryManager.getBoundedBuffer(address);
-        System.out.println("Bounded Buffer: "+buf);
-    }
 }
