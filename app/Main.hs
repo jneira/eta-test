67a94168cf4851573f3d05450bfcdf999657c0ef
jneira <atreyu.bbb@gmail.com>
2017-11-29 10:12:11 +0100

    cleaning master

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,6 @@
     FlexibleContexts,OverloadedStrings #-}
 module Main where
 
-main = putStrLn "cañá" >> putStrLn "नमस्ते" >> putStrLn "你好" >>
-       putStrLn "привет" -- >> putStrLn "Capital EX 🦎's first module!"
+main = putStrLn "Testing eta"
 
 
