6813f543e400d8446d48c6828a273548e7612cde
jneira <atreyu.bbb@gmail.com>
2018-03-12 14:02:42 +0100

    testing tryIOError

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,37 @@
 {-# LANGUAGE MagicHash,TypeFamilies,DataKinds,
-    FlexibleContexts,OverloadedStrings #-}
+    FlexibleContexts,OverloadedStrings, ScopedTypeVariables #-}
 module Main where
-import GHC.Foreign
-import Lib
+import Java
+import Java.Exception
+import qualified System.IO.Error as SysIOErr
+import Data.Proxy
+import Data.Typeable
+import qualified Control.Exception as EX 
 
-main = putStrLn $ show test
-       -- putStrLn "नमस्ते" >> putStrLn "你好" >>
-       --putStrLn "привет" >> putStrLn "Capital EX 🦎's first module!"
+foreign import java "@new" newFileNotFoundException ::  String -> FileNotFoundException
+foreign import java "@new" newException ::  String -> JException
 
-
-
+main = do
+  let ioEx = newFileNotFoundException "io ex"
+      ex   = newException "ex"
+  r <- SysIOErr.tryIOError (EX.throwIO ioEx)
+  case r of
+    Right() -> return ()
+    Left err -> putStrLn $ EX.displayException err
+  (r' :: Either JException ()) <- EX.try (EX.throwIO ex)
+  case r' of
+    Right() -> return ()
+    Left err -> putStrLn $ EX.displayException err
+  (r' :: Either EX.IOException ()) <- EX.try (EX.throwIO ioEx)
+  case r' of
+    Right() -> return ()
+    Left err -> putStrLn $ EX.displayException err
+--  r <- SysIOErr.tryIOError (EX.throwIO ex)
+--  case r of
+--    Right() -> return ()
+--    Left err -> putStrLn $ EX.displayException err
+  (r' :: Either JException ()) <- EX.try (EX.throwIO ioEx)
+  case r' of
+    Right() -> return ()
+    Left err -> putStrLn $ EX.displayException err
+  
