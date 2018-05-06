91640970765520410e40a736ed4fe2871facc928
jneira <atreyu.bbb@gmail.com>
2018-05-06 23:11:22 +0200

    default main

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,5 @@
 {-# LANGUAGE MagicHash,TypeFamilies,DataKinds,
     FlexibleContexts,OverloadedStrings, ScopedTypeVariables #-}
 module Main where
-import Java
-import Java.Exception
-import qualified System.IO.Error as SysIOErr
-import qualified Control.Exception as EX 
 
-
-foreign import java "@new" newFileNotFoundException ::  String -> FileNotFoundException
-foreign import java "@new" newException ::  String -> JException
-
-mkIOError type' msg = SysIOErr.ioeSetErrorString
-                      (SysIOErr.mkIOError type' "" Nothing Nothing) msg
-
-main = do
-  let ioEx = newFileNotFoundException "io ex"
-      ex   = newException "ex"
-      ioErr = mkIOError SysIOErr.doesNotExistErrorType "ioerr"
-  SysIOErr.catchIOError (EX.throwIO ioErr) $ putStrLn . show
-  catchJavaIOError (EX.throwIO ioErr) $ putStrLn . show
-
-  EX.catch (EX.throwIO ioEx) $ \ (e :: JException) -> putStrLn $ show  e
-  EX.catch (EX.throwIO ex) $ \ (e :: JException) -> putStrLn $ show e
-
-  catchJavaIOError (EX.throw ioEx) $ putStrLn . show
-  catchJavaIOError (readFile "x" >> return ()) $ putStrLn . show
+main = putStrLn "hello"
