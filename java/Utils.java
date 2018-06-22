2f9cc1bce33f5bd508f0f719c2df6cd45ddaa219
jneira <atreyu.bbb@gmail.com>
2018-06-22 14:02:13 +0200

    examples for docs

--- java/Utils.java
+++ java/Utils.java
@@ -1,21 +1,25 @@
-import eta_test.Lib;
+import eta.example.MyClass;
+import eta.example.Numbers;
+import eta.example.Counter;
+import eta.example.EtaCounter;
+
 public class Utils {
 
     public static void main(String args[]) throws Exception {
         System.out.println("======== RUNNING  ===============");
-        Lib.exportedFoo();
-        System.out.println("Bar:"+Lib.exportedBar());
-        System.out.println("Baz:"+Lib.exportedBaz());
-        EtaData d = new EtaData();
-        d.setCounter(10);
-        System.out.println(d.addToCounter(1));
-        System.out.println(d.getCounter());
-        System.out.println();
+        MyClass.sayHello();
+        System.out.println("Zero: "+ Numbers.zero());
+        System.out.println("One: "+ Numbers.one());
+        System.out.println("Two: "+ Numbers.addTwo(0));
+
+        EtaCounter d = new EtaCounter();
+        d.set(10);
+        System.out.println(d.decrement(5));
+        System.out.println(d.get());
         // This does not work, Lib has no constructors
         // Lib myLib = new Lib();
         // System.out.println(myLib.instanceMethod(1));
         System.out.println("=================================");
-        System.out.println(Lib.getInt(1));
     }
 
 }
