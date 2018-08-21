963fca30408a7fb7a60490891f530f4f8b4fd7fe
jneira <atreyu.bbb@gmail.com>
2018-08-21 08:23:31 +0200

    add verbose and open editor

--- test-dhall.cmd
+++ test-dhall.cmd
@@ -1,4 +1,5 @@
 ren eta-test.cabal eta-test.cabal.old
 ren etlas.dhall.old etlas.dhall
 rmdir dist /s /q
-etlas build > output.dhall.txt 2>&1
+etlas build -v3 > output.dhall.txt 2>&1
+ecw output.dhall.txt
