175461c7c05f69b0eb9fe435e7931c8373c35cfa
jneira <atreyu.bbb@gmail.com>
2018-10-23 08:22:29 +0200

    Tests for https://github.com/typelead/eta/issues/889

--- app/Main.hs
+++ app/Main.hs
@@ -6,4 +6,9 @@ import Java
 
 main = do
   i <- java $ createAndUseEtaCounter
+  java $ newJavaCounter >- setNumCounters 10
+  setNumCounters' 11
+  return $ setNumCounters'' 12 -- No effect
+  i' <- java $ getNumCounters
+  putStrLn $ show i'
   putStrLn $ "The value is " ++ show i  
